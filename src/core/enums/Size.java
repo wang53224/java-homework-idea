963530b15436403f90c17f7278f706281429805d
wang53224 <wang53224@126.com>
2020-07-14 21:18:46 +0800

    演示枚举类型的工作方式

---
+++ src/core/enums/Size.java
@@ -0,0 +1,15 @@
+package core.enums;
+
+public enum Size {
+    SMALL("S"),MEDIUM("M"),LARGE("L"), EXTRA_LARGE("XL");
+
+    private Size(String abbreviation){
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation(){
+        return abbreviation;
+    }
+
+    private String abbreviation;
+}
