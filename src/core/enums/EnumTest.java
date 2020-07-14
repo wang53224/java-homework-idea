0677ceaf9cba4cebb56a1d4838a935aa82b745b3
wang53224 <wang53224@126.com>
2020-07-14 21:24:41 +0800

    演示枚举类型的工作方式

--- src/core/enums/EnumTest.java
+++ src/core/enums/EnumTest.java
@@ -8,12 +8,14 @@ public class EnumTest {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a size (SMALL,MEDIUM,LARGE,EXTRA_LARGE): ");
         String input = in.next().toUpperCase();
-        Size size = Enum.valueOf(Size.class, input);
+        Size size = Enum.valueOf(Size.class, input);    //toString的逆方法,这里将size设置为Size.input
 
         System.out.println("size = " + size);
         System.out.println("abbreviation = " + size.getAbbreviation());
         if (size == Size.EXTRA_LARGE) {
             System.out.println("Good job -- you paid attention to the _.");
         }
+
+        System.out.println(Size.SMALL.toString());      ////toString方法,返回枚举常量名,这里返回字符串“SMALL”
     }
 }
