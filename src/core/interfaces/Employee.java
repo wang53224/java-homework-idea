74688c1291c10b8b6f1f9181a21141dfb9439542
wang53224 <wang53224@126.com>
2020-07-15 22:02:11 +0800

    添加演示接口注释

--- src/core/interfaces/Employee.java
+++ src/core/interfaces/Employee.java
@@ -4,8 +4,7 @@ public class Employee implements Comparable<Employee>{
     private String name;
     private double salary;
 
-    public Employee(String name, double salary)
-    {
+    public Employee(String name, double salary) {
         this.name = name;
         this.salary = salary;
     }
