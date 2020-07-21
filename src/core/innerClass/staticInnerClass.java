0a48d134d3b7a7be81d6ecb1b92f9617a0e5154a
wang53224 <wang53224@126.com>
2020-07-21 22:09:28 +0800

    静态内部类

---
+++ src/core/innerClass/staticInnerClass.java
@@ -0,0 +1,49 @@
+package core.innerClass;
+//计算数组中最大值和最小值
+
+public class staticInnerClass {
+    public static void main(String[] args) {
+        double[] d = new double[20];
+        for (int i = 0; i < d.length; i++)
+            d[i] = 100 * Math.random();
+        ArrayAlg.Pair p = ArrayAlg.minmax(d);
+        System.out.println("min = " + p.getFirst());
+        System.out.println("max = " + p.getSecond());
+    }
+}
+
+class ArrayAlg{
+    /**
+     * A pair of floating-point numbers
+     */
+
+    public static class Pair{
+        private double first;
+        private double second;
+
+        public Pair(double first, double second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public double getFirst(){
+            return first;
+        }
+        public double getSecond(){
+            return  second;
+        }
+    }
+
+    public static Pair minmax(double[] values) {
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (double v : values) {
+            if (min > v)    min = v;
+            if (max < v)    max = v;
+        }
+        return new Pair(min, max);
+    }
+}
+
+
