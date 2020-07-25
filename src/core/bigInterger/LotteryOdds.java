48ba74fc79703742f242604a4c20b0c8eee8940a
wang53224 <wang53224@126.com>
2020-07-25 22:28:50 +0800

    Java for循环计算中将概率

---
+++ src/core/bigInterger/LotteryOdds.java
@@ -0,0 +1,20 @@
+package core.bigInterger;
+
+import java.util.Scanner;
+
+public class LotteryOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many numbers do you need to draw? ");
+        int k = scanner.nextInt();
+
+        System.out.print("What is the highest number you can draw? ");
+        int n = scanner.nextInt();
+
+        int lotteryOdds = 1;
+        for (int i = 1; i <= k; i++) {
+            lotteryOdds = lotteryOdds * (n - i + 1) / i;
+        }
+        System.out.println("Your odds are 1 in " + lotteryOdds + ". Good luck!");
+    }
+}
