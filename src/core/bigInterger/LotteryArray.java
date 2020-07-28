45ab4faee5fcc2fe73dd43098c6a58fc6fc3be22
wang53224 <wang53224@126.com>
2020-07-28 21:45:44 +0800

    Java 不规则数组产生三角形

--- src/core/bigInterger/LotteryArray.java
+++ src/core/bigInterger/LotteryArray.java
@@ -9,17 +9,19 @@ public class LotteryArray {
 
         // allocate triangular array
         int[][] odds = new int[N_MAX + 1][];
-        for (int n = 0; n < N_MAX; n++) {
+        for (int n = 0; n <= N_MAX; n++) {
             odds[n] = new int[n + 1];
         }
 
         //fill triangular array
         for (int n = 0; n < odds.length; n++) {
-            for (int k = 0; k < odds[n].length; k++) {
+            int k = 0;
+            while (k < odds[n].length) {
                 int lotteryOdds = 1;
                 for (int i = 1; i <= k; i++)
-                        lotteryOdds = lotteryOdds * (n - i + 1) / i;
+                    lotteryOdds = lotteryOdds * (n - i + 1) / i;
                 odds[n][k] = lotteryOdds;
+                k++;
             }
         }
 
