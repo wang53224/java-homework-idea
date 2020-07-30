19a7ef4a4cf28510683bf411dd7fd57553de95fa
wang53224 <wang53224@126.com>
2020-07-30 21:56:04 +0800

    Java 输入

--- src/com/google/InputText.java
+++ src/com/google/InputText.java
@@ -1,14 +1,27 @@
 package com.google;
 
+import java.io.Console;
 import java.util.Scanner;
 
 public class InputText {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+
+        // get first input
         System.out.print("Input your name: ");
         String name = in.nextLine();
+
+        // get second input
         System.out.print("Input your old: ");
         int age = in.nextInt();
-        System.out.println("Hello," + name + ".Next age,you'll be " + (age + 1));
+
+        // display output on console
+        System.out.println("Hello," + name + ".Next year,you'll be " + (age + 1));
+
+        // 读取密码 NullPointerException
+        Console console = System.console();
+        String username = console.readLine("User name: ");
+        char[]  password = console.readPassword("Password: ");
+
     }
 }
