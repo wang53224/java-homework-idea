c6c7163a64b3706a0e55e980288afbf004a9895f
wang53224 <wang53224@126.com>
2020-08-16 21:45:00 +0800

    不同利率下的投资增长情况,增加注释

--- src/core/Retirement/CompoundInterest.java
+++ src/core/Retirement/CompoundInterest.java
@@ -10,16 +10,19 @@ public class CompoundInterest {
         final int NRATES = 6;
         final int NYEARS = 10;
 
+        //set interest rates to 10 ... 15%
         double[] interestRate = new double[NRATES];
         for (int j = 0; j < interestRate.length; j++) {
             interestRate[j] = (STARTRATE + j) / 100.0;
         }
         double[][] balances = new double[NYEARS][NRATES];
 
+        // set initial balances to 10000
         for (int j = 0; j < balances[0].length; j++) {
             balances[0][j] = 10000;
         }
 
+        // compute interest for future years
         for (int i = 1; i < balances.length; i++) {
             for (int j = 0; j < balances[0].length; j++) {
                 double oldBalance = balances[i-1][j];
@@ -27,11 +30,16 @@ public class CompoundInterest {
                 balances[i][j] = oldBalance + interest;
             }
         }
+
+        // print one row of interest rates
         for (int j = 0; j < interestRate.length; j++) {
             System.out.printf("%9.0f%%", 100 * interestRate[j]);
         }
         System.out.println();
+
+        // print balance table
         for (double[] row : balances) {
+            // print table row
             for (double b : row)
                 System.out.printf("%10.2f", b);
 
